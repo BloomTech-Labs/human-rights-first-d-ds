55ecfd2151e5cec396f322755b5398a2933f54d1
popkdodge <popkdodge@gmail.com>
2020-11-02 12:08:51 -0500

    Initial pull request push

---
+++ project/app/api/us_map.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException
+import random
+from pydantic import BaseModel, Field, validator
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+df = pd.read_csv('', index_col=[0])
+def map_function(df, start_date, end_date, sort_by:str= "Armed/Unarmed"):
+    # Selection of timeframes
+    df = df.copy()
+    mask =  (df['Date of Incident (month/day/year)'] > start_date) & ( df['Date of Incident (month/day/year)'] <= end_date)
+    df = df.loc[mask]
+    mapbox_access_token = 'pk.eyJ1IjoicG9wa2RvZGdlIiwiYSI6ImNrZDdvZDFtbDAwNmwycW9xazQycWpldTYifQ.33ELrqLko1a0dHHEkSsxNw'
+    if sort_by == "Armed/Unarmed":
+        color="Unarmed/Did Not Have an Actual Weapon"
+    if sort_by == "Demographic":
+        color="Victim's race"
+    if sort_by == "Gender":
+        color="Victim's gender"
+    px.set_mapbox_access_token(mapbox_access_token)
+    fig = px.scatter_mapbox(df,
+                            lat=df.lon,
+                            lon=df.lat,
+                            zoom=1,
+                            hover_name= "Victim's name",
+                            hover_data= ['Victim\'s age','Victim\'s gender','Victim\'s race',"State","City"],
+                            color = color,
+                            title=f"Police Shooting Between {start_date} and {end_date}"
+                           )
+    fig.update_layout(mapbox_style="open-street-map",
+                      mapbox_zoom=3, mapbox_center = {"lat": 37.0902, "lon": -95.7129})
+    return fig.to_json()
+
+router = APIRouter()
+
+class Input(BaseModel):
+    """Use this to mode pare request body JSON."""
+    start_date: str
+    end_date: str
+    sort_by: str
+@router.post('/us_map')
+async def us_map(item: Input):
+    """
+    ### Request Body
+    = "start_date" : string 'yy-mm-dd' format.
+    = "end_date" : string 'yy-mm-dd' format.
+    - "sort_by: string this will determing how the
+    lengend will sort data default option:
+        -armed/unarmed
+    possible options:
+        "Demographic",
+        "Victim's gender",
+
+
+    ### Response
+    - `prediction`: boolean, at random
+    - `predict_proba`: float between 0.5 and 1.0, 
+    representing the predicted class's probability
+
+    Replace the placeholder docstring and fake predictions with your own model.
+    """
+    return map_function(df, start_date, end_date, "Demographic")
